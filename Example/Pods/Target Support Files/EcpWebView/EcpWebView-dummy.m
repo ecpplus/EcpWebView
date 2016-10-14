9c2ccb95341cee7076723ebb15756f729ce6539e
chu <ecpplus@gmail.com>
2016-10-15 00:15:00 +0900

    delete Pods

--- Example/Pods/Target Support Files/EcpWebView/EcpWebView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_EcpWebView : NSObject
-@end
-@implementation PodsDummy_EcpWebView
-@end
--- Example/Pods/Target Support Files/Pods-EcpWebViewExample/Pods-EcpWebViewExample-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_EcpWebViewExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_EcpWebViewExampleVersionString[];
-
